31e9dcd4d850f8bcc7b3856ee50a37936c5776c6
Jikkata <tenevj@gmail.com>
2024-11-13 15:02:25 +0200

    пхп_сцрипт

---
+++ contact/php_script.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "zhivko.tenev@mibdistillery.com";
+    $subject = "New Message from Contact Form";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email.";
+    }
+}
+?>
