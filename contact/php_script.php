51b6b5144201d1e1c8541ba3f55ec02339e1983d
Jikkata <tenevj@gmail.com>
2024-11-13 17:29:38 +0200

    prosek.mailer

--- contact/php_script.php
+++ contact/php_script.php
@@ -7,12 +7,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $to = "zhivko.tenev@mibdistillery.com";
     $subject = "New Message from Contact Form";
     $body = "Name: $name\nEmail: $email\nMessage: $message";
-    $headers = "From: $email";
 
-    if (mail($to, $subject, $body, $headers)) {
+    $smtp_host = "smtp.gmail.com";
+    $smtp_port = 587;
+    $smtp_username = "prosek.mailer@gmail.com";
+    $smtp_password = "zsoxrqjjtxfjhrzo";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    $transport = (new Swift_SmtpTransport($smtp_host, $smtp_port))
+        ->setUsername($smtp_username)
+        ->setPassword($smtp_password);
+    $mailer = new Swift_Mailer($transport);
+    $message = (new Swift_Message())
+        ->setSubject($subject)
+        ->setFrom([$smtp_username => $name])
+        ->setTo([$to])
+        ->setBody($body);
+
+    if ($mailer->send($message)) {
         echo "Email sent successfully!";
     } else {
         echo "Error sending email.";
+        error_log(print_r(error_get_last(), true));
     }
+} else {
+    http_response_code(405);
+    echo "Method Not Allowed";
 }
 ?>
