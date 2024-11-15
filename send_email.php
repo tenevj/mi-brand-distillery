56011c99758961d9d4da9cda30ba3080a2cc8ecb
Jikkata <tenevj@gmail.com>
2024-11-15 12:29:56 +0200

    workign email from contact_us

--- send_email.php
+++ send_email.php
@@ -1,37 +1,50 @@
-
-
 <?php
-$to = "zhivko.tenev@mibdistillery.com, ztenev@prosek.com";
-$subject = "HTML email";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
+    $to = "zhivko.tenev@mibdistillery.com, ztenev@prosek.com";
+    $subject = "New Message from Contact Form";
+    $email_body = "
+        <html>
+        <head>
+            <title>New Message from Contact Form</title>
+        </head>
+        <body>
+            <p>You have received a new message from the contact form:</p>
+            <table>
+                <tr>
+                    <th>Name:</th>
+                    <td>$name</td>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td>$email</td>
+                </tr>
+                <tr>
+                    <th>Phone:</th>
+                    <td>$phone</td>
+                </tr>
+                <tr>
+                    <th>Message:</th>
+                    <td>$message</td>
+                </tr>
+            </table>
+        </body>
+        </html>
+    ";
 
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: <tenevj@gmail.com>' . "\r\n";
+    $headers .= 'Cc: undeddo@gmail.com' . "\r\n";
 
-// More headers
-$headers .= 'From: <tenevj@gmail.com>' . "\r\n";
-$headers .= 'Cc: undeddo@gmail.com' . "\r\n";
-
-mail($to,$subject,$message,$headers);
+    if (mail($to, $subject, $email_body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
 ?>
