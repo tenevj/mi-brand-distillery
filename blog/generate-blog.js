d43f4034147e14c90817a7ebb3beb4d259189d32
Jikkata <tenevj@gmail.com>
2024-12-06 18:49:47 +0200

    respect new lines in markdown

--- blog/generate-blog.js
+++ blog/generate-blog.js
@@ -2,54 +2,7 @@
 const fs = require('fs'); // To work with the file system
 const path = require('path'); // To handle and transform file paths
 const yaml = require('js-yaml'); // To parse YAML front matter in markdown files
-
-// Function to generate the list of blog posts
-/* function generateBlogPosts() {
-    const blogDir = path.join(__dirname, 'posts'); // Directory where markdown files are stored
-    const posts = []; // Array to hold the blog posts
-
-    // Check if the 'posts' directory exists
-    if (fs.existsSync(blogDir)) {
-        const files = fs.readdirSync(blogDir); // Get all files in the 'posts' directory
-        
-        // Loop through each file in the directory
-        files.forEach(file => {
-            console.log('Processing file:', file);  // Debugging line
-            // Process only markdown (.md) files
-            if (file.endsWith('.md')) {
-                const filePath = path.join(blogDir, file); // Get the full path of the file
-                const content = fs.readFileSync(filePath, 'utf8'); // Read the content of the markdown file
-
-                // Regex to extract front matter (YAML) from markdown files
-                const frontMatterRegex = /^---([\s\S]*?)---/;
-                const match = content.match(frontMatterRegex);
-
-                // If front matter exists, parse it
-                if (match) {
-                    try {
-                        // Use js-yaml to parse the front matter
-                        const frontMatter = yaml.load(match[1]);
-                        
-                        // Push the parsed data into the posts array
-                        posts.push({
-                            title: frontMatter.title,
-                            date: frontMatter.date,
-                            content: content.replace(frontMatterRegex, ''), // Remove the front matter from the content
-                            //image: frontMatter.image || 'default.jpg' // Default image if not specified in front matter
-                            image: `images/${frontMatter.image || 'default.jpg'}`
-
-                        });
-                    } catch (error) {
-                        console.error("Error parsing YAML front matter in file:", file);
-                        console.error(error);
-                    }
-                }
-            }
-        });
-    }
-
-    return posts; // Return the array of blog posts
-} */
+const marked = require('marked');
 
 //LATEST CHANGE 2024-12-06 17:57:08
     const posts = []; // Array to hold the blog posts
@@ -84,11 +37,12 @@ function generateBlogPosts() {
                             ? `images/${frontMatter.image}` 
                             : 'images/default.jpg';
                         
+                        
                         // Push the parsed data into the posts array
                         posts.push({
                             title: frontMatter.title,
                             date: frontMatter.date,
-                            content: content.replace(frontMatterRegex, ''), // Remove the front matter from the content
+                            content: content.replace(frontMatterRegex, '').replace(/\n/g, '<br>'), // Remove the front matter from the content; 2024-12-06 18:49:06 respect new lines 
                             image
                         });
                     } catch (error) {
